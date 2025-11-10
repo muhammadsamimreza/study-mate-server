745c6d5ae11041660c55372f52fa769df16aabe5
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-11-10 22:19:34 +0600

    get one info for detalis, API created

--- index.js
+++ index.js
@@ -1,42 +1,77 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 3000;
-// FNdJ1OK8w3KYDFwv
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqrplnr.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, { 
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-async function run() { 
+async function run() {
   try {
     await client.connect();
 
+    const db = client.db("study-Partners");
+    const partners = db.collection("partnersCollection");
+
+    // all Partners get API
+    app.get("/allpartners", async (req, res) => {
+      const result = await partners.find().toArray();
+      console.log(result);
+
+      res.send(result);
+    });
+
+    // Top Ratted Partner get API
+
+    app.get("/top-partners", async (req, res) => {
+      try {
+        const result = await partners
+          .find()
+          .sort({ rating: -1 })
+          .limit(3)
+          .toArray();
+        // console.log(result);
+        res.send(result);
+      } catch (err) {
+        console.error("Error fetching top partners:", err);
+        res.status(500).send({ message: "Error fetching top partners" });
+      }
+    });
+
+
+    // get One Partner 
+
+    app.get('/partnerDetails/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = { _id: new ObjectId(id)}
+            const result = await partners.findOne(query)
+            console.log(result)
+            res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 // Test route
 app.get("/", (req, res) => {
